13ae3c90ab9ea9bf27c114da72d50af074919bd3
Joel W. Blomster <joel.blomster@gilion.com>
2024-08-04 18:55:11 +0200

    result

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -91,7 +91,7 @@ impl Writer {
 }
 
 impl fmt::Write for Writer {
-    fn write_str(&mut self, s: &str) -> Result<(), Error> {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
         self.write_string(s);
         Ok(())
     }
